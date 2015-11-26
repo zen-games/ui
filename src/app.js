eb9a8d79627aff92e2bb87c9ba844b45cd251949
Alex Wilmer <alex.wilmer@oicr.on.ca>
2015-11-26 12:20:49 -0500

    react starter

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
-let msg = document.createElement(`h1`)
-msg.innerText = `Hello world!`
-document.body.appendChild(msg)
+import React from 'react'
+import { render } from 'react-dom'
+
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+
+let App = () =>
+  <div>
+    <h1>Hello world!</h1>
+    <Tabs>
+      <TabList>
+        <Tab>Foo</Tab>
+        <Tab>Bar</Tab>
+        <Tab>Baz</Tab>
+      </TabList>
+
+      <TabPanel>
+        <h2>Hello from Foo</h2>
+      </TabPanel>
+
+      <TabPanel>
+        <h2>Hello from Bar</h2>
+      </TabPanel>
+      
+      <TabPanel>
+        <h2>Hello from Baz</h2>
+      </TabPanel>
+    </Tabs>
+  </div>
+
+render(<App />, document.getElementById('app'))
